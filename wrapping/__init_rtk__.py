2d0d2429ec96bd72dbbf7f5ee1b213238b19a514
Axel Garcia <axel.garcia1997@gmail.com>
2025-06-27 16:04:41 +0200

    ENH: Add rtkdigisensgeometry python application

--- wrapping/__init_rtk__.py
+++ wrapping/__init_rtk__.py
@@ -26,6 +26,7 @@ _app_modules = [
     "rtkbackprojections",
     "rtkbioscangeometry",
     "rtkconjugategradient",
+    "rtkdigisensgeometry",
     "rtkdrawgeometricphantom",
     "rtkdrawshepploganphantom",
     "rtkelektasynergygeometry",
